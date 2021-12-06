a94106dc35a9e76bb4adbd9f44ad0f29a82578a2
Kevin Attinger <kevin.attinger@hs-furtwangen.de>
2021-12-06 14:02:10 +0100

    remove critical stuff

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueQrcodeReader from 'vue-qrcode-reader';
-import VueGeolocation from 'vue-browser-geolocation';
-import * as VueGoogleMaps from 'vue2-google-maps';
 import Home from '../views/Home.vue';
 import Map from '../views/Map.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueQrcodeReader);
-Vue.use(VueGeolocation);
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyC-nvXVO6b3bAqGgXyGm9f_tLJwraoOCAQ',
-    },
-})
 
 const routes = [
     {
